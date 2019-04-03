ccafc38e46ca63d2f2ec7cc6f6aab008e8b01aa3
Paulo Olayres <pjolayres@live.com>
2019-04-03 15:39:45 +0400

    Initial commit

---
+++ components/nav.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import Link from 'next/link';
+
+interface LinkItem {
+  href: string,
+  label: string,
+  key?: string
+}
+
+const linkItems = [
+  {
+    href: 'https://github.com/segmentio/create-next-app',
+    label: 'Github'
+  } as LinkItem
+].map(linkItem => {
+  linkItem.key = `nav-link-${linkItem.href}-${linkItem.label}`;
+  return linkItem;
+});
+
+const Nav = () => (
+  <nav>
+    <ul>
+      <li>
+        <Link prefetch href="/">
+          <a>Home</a>
+        </Link>
+      </li>
+      <ul>
+        {linkItems.map(({ key, href, label }) => (
+          <li key={key}>
+            <Link href={href}>
+              <a>{label}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </ul>
+
+    <style jsx>{`
+      :global(body) {
+        margin: 0;
+        font-family: -apple-system, BlinkMacSystemFont, Avenir Next, Avenir, Helvetica, sans-serif;
+      }
+      nav {
+        text-align: center;
+      }
+      ul {
+        display: flex;
+        justify-content: space-between;
+      }
+      nav > ul {
+        padding: 4px 16px;
+      }
+      li {
+        display: flex;
+        padding: 6px 8px;
+      }
+      a {
+        color: #067df7;
+        text-decoration: none;
+        font-size: 13px;
+      }
+    `}</style>
+  </nav>
+);
+
+export default Nav;
