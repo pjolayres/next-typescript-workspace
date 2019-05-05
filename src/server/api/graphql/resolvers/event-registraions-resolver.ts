790c19a4f40321715039012844912b241ee13da0
Paulo Olayres <pjolayres@live.com>
2019-05-05 21:36:41 +0400

    Implemented EventRegistration.EventItem field resolver

--- src/server/api/graphql/resolvers/event-registraions-resolver.ts
+++ src/server/api/graphql/resolvers/event-registraions-resolver.ts
@@ -1,9 +1,12 @@
-import { Query, Args, Resolver } from 'type-graphql';
+import { Query, Args, Resolver, FieldResolver, Root, Ctx } from 'type-graphql';
 
 import EventRegistrationsRepository from '../../../repositories/event-registrations-repository';
+import EventItemsRepository from '../../../repositories/event-items-repository';
 import EventRegistration from '../../../entities/event-registration';
 import FetchListArgs from '../types/fetch-list-args';
 import Utilities from '../../../../shared/utilities';
+import { Context } from '../../../../../types';
+import EventItem from '../../../entities/event-item';
 
 @Resolver(_of => EventRegistration)
 export default class EventRegistrationsResolver {
@@ -16,5 +19,20 @@ export default class EventRegistrationsResolver {
     const items = await this.eventRegistrationsRepository.getItems(options);
 
     return items;
+  }
+
+  @FieldResolver()
+  async EventItem(@Root() eventRegistration: EventRegistration, @Ctx() context: Context) {
+    const dataLoader = context.dataLoaderFactory.getDataLoader<EventItem, string>(EventItem, 'EventItemId', async (eventItemIds: string[]) => {
+      const eventItemsRepostory = new EventItemsRepository();
+      const items = await eventItemsRepostory.getItemsByIds(eventItemIds);
+      const sortedItems = eventItemIds.map(key => items.find(item => key === item.EventItemId));
+
+      return sortedItems;
+    });
+
+    const result = await dataLoader.load(eventRegistration.EventItemId);
+
+    return result;
   }
 }
